0482d3e495afbe5fc77bc0ed4f2678dfbfd93016
yewenjuan <583619302@qq.com>
2020-12-30 16:01:39 +0800

    美食大全轮播开发

--- src/Home/cookbook/Cookbook.jsx
+++ src/Home/cookbook/Cookbook.jsx
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 
 import { Container } from "./StyleCookBook";
+import Swiper from "./Swiper";
 
 export default class Cookbook extends Component {
   render() {
     return (
       <Container>
         <header>美食大全</header>
+        <Swiper></Swiper>
       </Container>
     )
   }
---
+++ src/Home/cookbook/Swiper.jsx
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import { Carousel } from "antd-mobile";
+
+import img1 from "@assets/images/pic1.jpg";
+import img2 from "@assets/images/pic2.jpg";
+import img3 from "@assets/images/pic3.jpg";
+
+export default class Swiper extends Component {
+  render() {
+    return (
+      <div>
+        <Carousel
+          autoplay={true}
+          infinite
+        >
+          <img src={img1} alt=""/>
+          <img src={img2} alt=""/>
+          <img src={img3} alt=""/>
+        </Carousel>
+      </div>
+    )
+  }
+}
